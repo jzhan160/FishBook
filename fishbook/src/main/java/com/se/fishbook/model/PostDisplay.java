93779f1b71d565234dcba4868aeebaeb3b2569f2
adaohan <ba1000@syr.edu>
2018-11-09 21:21:22 -0500

    连了一下

---
+++ fishbook/src/main/java/com/se/fishbook/model/PostDisplay.java
@@ -0,0 +1,22 @@
+package com.se.fishbook.model;
+
+public class PostDisplay {
+    private Post post;
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+}
