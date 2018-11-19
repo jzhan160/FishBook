149c42b4313c90d585d4d71a7faa3a0722568a16
adaohan <ba1000@syr.edu>
2018-11-19 16:53:17 -0500

    点赞和点赞推送

--- fishbook/src/main/java/com/se/fishbook/service/impl/NotificationServiceImpl.java
+++ fishbook/src/main/java/com/se/fishbook/service/impl/NotificationServiceImpl.java
@@ -1,8 +1,11 @@
 package com.se.fishbook.service.impl;
 
 import com.se.fishbook.mapper.NotificationMapper;
+import com.se.fishbook.mapper.PostMapper;
 import com.se.fishbook.model.Notification;
 import com.se.fishbook.model.NotificationExample;
+import com.se.fishbook.model.Post;
+import com.se.fishbook.model.User;
 import com.se.fishbook.service.NotificationService;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,9 @@ import java.util.List;
 public class NotificationServiceImpl implements NotificationService {
     @Resource
     private NotificationMapper notificationMapper;
+
+    @Resource
+    private PostMapper postMapper;
 
     @Override
     public List<Notification> showUnreadNotifications(Integer UserId) {
@@ -27,5 +33,23 @@ public class NotificationServiceImpl implements NotificationService {
         NotificationExample ne = new NotificationExample();
         ne.createCriteria().andReceiveridEqualTo(UserId).andViewedEqualTo(new Byte("1"));
         return notificationMapper.selectByExample(ne);
+    }
+
+    @Override
+    public void newLikes(Integer postId, Integer userId) {
+        Notification n = new Notification();
+        n.setEvent("Like");
+        n.setTriggerid(userId);
+        n.setReceiverid(postMapper.selectByPrimaryKey(postId).getAuthorid());
+        notificationMapper.insert(n);
+    }
+
+    @Override
+    public void newComments(Integer postId, Integer userId) {
+        Notification n = new Notification();
+        n.setEvent("Comment");
+        n.setTriggerid(userId);
+        n.setReceiverid(postMapper.selectByPrimaryKey(postId).getAuthorid());
+        notificationMapper.insert(n);
     }
 }
